d5e8df80454f5bea5ccdb8a9115ce7234fe1b380
Jim Doyle <indellibleink@gmail.com>
2020-04-24 13:15:23 -0400

    data types and math operators

---
+++ dataTypes.py
@@ -0,0 +1,36 @@
+# Basic Data Types
+
+# Integer
+number=20
+# float
+decimalNumber=10.2
+# Boolean
+choice=True
+# None value
+none=None
+
+# print type of variable name
+print (type(number),type(decimalNumber),type(choice),type(none))
+
+# prints varible names
+print (number,decimalNumber,choice,none)
+
+# is a variable a type returns True or False
+print (none is None)
+
+# is not variable type returns Ture or False
+print (number is not None)
+
+# Math
+# Typical math useage: +,-,*,% implied
+# / returns a float
+print (3/4)
+
+# // returns an Integer
+print (3//4)
+
+# exponents
+print (3**4)
+
+# Integer and floats can be mixed in math operations
+print (number + decimalNumber)
