cd50222b32ae9940aea011f8883c82b5b3325470
Jim Doyle <indellibleink@gmail.com>
2020-04-24 12:30:35 -0400

    simplec ode

--- resistance.py
+++ resistance.py
@@ -1,2 +1,7 @@
+# this is a commented line
+# semicolons can be used, but it;s not practised.
 print('Resistence is futile!')
 print(3**4)
+
+# semicolon can be used to compound statements
+print('This string and math are on a single line'); print(2**4)
