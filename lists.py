14b2fde4c89f771f64b01e445372cf4ad1c545c1
Jim Doyle <indellibleink@gmail.com>
2020-04-24 20:38:23 -0400

    Lists class

--- lists.py
+++ lists.py
@@ -4,8 +4,9 @@
 # A list can contain items of different types: [1, 'here', 2.3]
 # lists works the same as in other languages
 # mutable and dynamic
+# String methods works the same on lists
 
 stuff=[1,'whoopee',2.5,1000,'Careerdev',3.333333]
 
 # in works the same as strings
-print(1 in stuff)
+print(1 in stuff) # True
