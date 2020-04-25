3694b5b895d9dccf6e1872f7b70b81466fd2b266
Jim Doyle <indellibleink@gmail.com>
2020-04-24 20:01:53 -0400

    Lists class

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Lists
+
+# A list is enclosed by []
+# A list can contain items of different types: [1, 'here', 2.3]
+# lists works the same as in other languages
+# mutable and dynamic
+
+stuff=[1,'whoopee',2.5,1000,'Careerdev',3.333333]
+
+# in works the same as strings
+print(1 in stuff)
